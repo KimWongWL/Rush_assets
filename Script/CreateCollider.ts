c5867c77cb59bd7b19290c5c5aca0d3b78d2c9e6
KimWongWL <kim.wlwong@gmail.com>
2025-03-06 00:54:59 +0800

    change
    -remark
    suffer in raycast
    getworldposition for ray
    find way to match the position and world position then done

--- Script/CreateCollider.ts
+++ Script/CreateCollider.ts
@@ -1,4 +1,4 @@
-import { _decorator, BoxCollider2D, Component, instantiate, Node, Prefab, RigidBody2D, Size, TiledMap, Vec2, Vec3, Sprite } from 'cc';
+import { _decorator, BoxCollider2D, Component, instantiate, Node, Prefab, RigidBody2D, Size, TiledMap, Vec2, v2, Sprite, UITransform } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('CreateCollider')
@@ -9,52 +9,60 @@ export class CreateCollider extends Component {
     @property({ type: Prefab })
     public OneWayWall: Prefab = null;
 
+    scale = 1.01;
+
     makeWallColliders() {
         let tiledMap = this.node.getComponent(TiledMap);
-        let tileSize = tiledMap.getTileSize();
+        let tileSize = tiledMap.getTileSize() ;
         let layer = tiledMap.getLayer('wall');
         let layerSize = layer.getLayerSize();
-        let scale = 1.01;
 
         for (let i = 0; i < layerSize.width; i++) {
             for (let j = 0; j < layerSize.height; j++) {
                 let tile = layer.getTiledTileAt(i, j, true);
 
                 if (tile.grid != 0) {
+                    tile.node.name = this.node.name + '_Wall_' + i + '_' + j;
                     let rigidBody = tile.node.addComponent(RigidBody2D);
                     rigidBody.type = 0;
                     let collider = tile.node.addComponent(BoxCollider2D);
                     collider.offset = new Vec2(tileSize.width / 2, tileSize.height / 2);
-                    collider.size = new Size(tileSize.x * scale, tileSize.y * scale);  
+                    collider.size = new Size(tileSize.x * this.scale, tileSize.y * this.scale);  
                     collider.group = 2; //wall  //start from 1
                     collider.friction = 0;
                     collider.apply();
+                    let ui = tile.node.getComponent(UITransform);
+                    ui.setContentSize(collider.size);
+                    ui.setAnchorPoint(0, 0);
 
                     //show collider
-                    let ui = instantiate(this.colliderUI);
-                    this.node.addChild(ui);
-                    ui.setPosition(tile.node.getPosition());
-                    ui.translate(new Vec3(tileSize.width / 2, tileSize.height / 2, 0));
-                    ui.scale.multiplyScalar(scale);
+                    //let ui = instantiate(this.colliderUI);
+                    //this.node.addChild(ui);
+                    //ui.setPosition(tile.node.getPosition());
+                    //ui.translate(new Vec3(tileSize.width / 2, tileSize.height / 2, 0));
+                    //ui.scale.multiplyScalar(scale);
 
                     // make ground collider too
                     if (this.node.name == 'Underground' && j == 10 && i == 0) {
                         for (let k = 1; k < 100; k++) {
                             //show collider
-                            let ui = instantiate(this.colliderUI);
-                            this.node.addChild(ui);
-                            ui.setPosition(tile.node.getPosition());
-                            ui.translate(new Vec3(tileSize.width / 2 - tileSize.width * k, tileSize.height / 2, 0));
-                            ui.scale.multiplyScalar(scale);
+                            //let ui = instantiate(this.colliderUI);
+                            //this.node.addChild(ui);
+                            //ui.setPosition(tile.node.getPosition());
+                            //ui.translate(new Vec3(tileSize.width / 2 - tileSize.width * k, tileSize.height / 2, 0));
+                            //ui.scale.multiplyScalar(scale);
 
                             let rigidBody = tile.node.addComponent(RigidBody2D);
                             rigidBody.type = 0;
                             let collider = tile.node.addComponent(BoxCollider2D);
                             collider.offset = new Vec2(tileSize.width / 2 - tileSize.width * k, tileSize.height / 2);
-                            collider.size = new Size(tileSize.x * scale, tileSize.y * scale);
+                            collider.size = new Size(tileSize.x * this.scale, tileSize.y * this.scale);
                             collider.group = 2; //wall  //start from 1
                             collider.friction = 0;
                             collider.apply();
+                            let ui = tile.node.getComponent(UITransform);
+                            ui.setContentSize(collider.size);
+                            ui.setAnchorPoint(0, 0);
                         }
                     }
                 }
@@ -70,27 +78,24 @@ export class CreateCollider extends Component {
         let tileSize = tiledMap.getTileSize();
         let layer = tiledMap.getLayer('oneway');
         let layerSize = layer.getLayerSize();
-        let scale = 1.01;
 
         for (let i = 0; i < layerSize.width; i++) {
             for (let j = 0; j < layerSize.height; j++) {
                 let tile = layer.getTiledTileAt(i, j, true);
-
                 if (tile.grid != 0) {
                     let oneway = instantiate(this.OneWayWall);
                     this.node.addChild(oneway);
+                    oneway.name = this.node.name + '_OneWayWall_' + i + '_' + j;
                     oneway.setPosition(tile.node.getPosition());
-                    //oneway.translate(new Vec3(tileSize.width / 2, tileSize.height / 2, 0));
-                    oneway.scale.multiplyScalar(scale);
-
+                    oneway.scale.multiplyScalar(this.scale);
+                    let ui = oneway.getComponent(UITransform);
+                    ui.setContentSize(new Size(tileSize.x * this.scale, tileSize.y * this.scale));
+                    ui.setAnchorPoint(0,0);
                     //show ui
-                    oneway.getComponent(Sprite).enabled = true;
-                    oneway.getComponentInChildren(Sprite).enabled = true;
+                    //oneway.getComponent(Sprite).enabled = true;
+                    //oneway.getComponentInChildren(Sprite).enabled = true;
                 }
-                else {
-                    tile.node.destroy();
-                }
-
+                tile.node.destroy();
             }
         }
     }
--- Script/Shooter.ts
+++ Script/Shooter.ts
@@ -1,4 +1,4 @@
-import { _decorator, Animation, Component, Node, Prefab, instantiate, geometry, math, v3, PhysicsSystem } from 'cc';
+import { _decorator, Animation, Component, Node, EPhysics2DDrawFlags, v2, PhysicsSystem2D, ERaycast2DType, v3, UITransform, Prefab, instantiate, Vec2, Sprite, math } from 'cc';
 import { PlayerController } from './PlayerController';
 const { ccclass, property } = _decorator;
 
@@ -7,41 +7,122 @@ export class Shooter extends Component {
 
     @property({ type: Node })
     public player: Node = null;
+    @property({ type: Prefab })
+    public ui: Prefab = null;
     bullet: Node = null;
     animation:Animation;
     cooldown = 2;
     cd = 2;
+
+    range = 500;
+    x = this.range;
+    y = this.range;
+    xDir = -1;
+    yDir = 0;
+    path = 0;
+    offset;
 
     @property
     public animEnd: boolean = false;
     @property
     public canShoot: boolean = false;
 
-    ray = new geometry.Ray(0, -1, 0, 0, 1, 0);
-
     onLoad() {
         this.animation = this.node.getComponent(Animation);
+
+        PhysicsSystem2D.instance.enable = true;
+        PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.Shape;
+        this.offset = v2(0, this.node.getComponent(UITransform).contentSize.y / 2);
+
+        //this.x = -500;
+
+        let targetPos = v2(this.node.position.x + this.x, this.node.position.y + this.y);
+        let oriPos = v2(this.node.position.x, this.node.position.y);
+
+        this.player.setPosition(v3(targetPos.x, targetPos.y, 0));
+
+        {
+            let ui = instantiate(this.ui);
+            this.node.addChild(ui);
+            ui.setWorldPosition(v3(oriPos.x, oriPos.y, 0));
+            ui.getComponent(Sprite).color = math.color(0, 0, 255, 255);
+        }
+        {
+            let ui = instantiate(this.ui);
+            this.node.addChild(ui);
+            ui.setWorldPosition(v3(targetPos.x, targetPos.y, 0));
+            ui.getComponent(Sprite).color = math.color(255,0,0,255);
+        }
+        console.log(this.node.position);
+        console.log(this.node.getWorldPosition());
+        console.log(oriPos);
+        console.log(targetPos);
+
+        let results = PhysicsSystem2D.instance.raycast(oriPos, targetPos, ERaycast2DType.All);
+        if (results) {
+            //console.log(results.length);
+            for (let i = 0; i < results.length; i++) {
+                let result = results[i];
+                let collider = result.collider;
+                let point = result.point;
+
+                console.log('ray hit ' + collider.node.name + ' from ' + collider.node.name + ' at ' + point);
+
+                let ui = instantiate(this.ui);
+                this.node.addChild(ui);
+                ui.setWorldPosition(v3(point.x, point.y, 0));
+            }
+        }
     }
 
     update(deltaTime: number) {
 
-        let mask = 0xffffffff;
-        let maxDistance = 1000;
-        let queryTrigger = true;
+        return;
 
-        let bResult = PhysicsSystem.instance.raycast(this.ray);
-        console.log(bResult);
-        if (bResult) {
-            let results = PhysicsSystem.instance.raycastResults;
+        if (1) {
+            this.x += deltaTime * this.xDir * 250;
+            this.y += deltaTime * this.yDir * 250;
+            if (this.x < -this.range || this.x > this.range ||
+                this.y < -this.range || this.y > this.range) {
 
+                if (this.xDir != 0) {
+                    this.x = this.range * this.xDir;
+                } else {
+                    this.y = this.range * this.yDir;
+                }
+
+                this.path++;
+                let changeDir = 1;
+                if (this.path == 2) {
+                    //change direction
+                    this.path = 0;
+                    changeDir = -1;
+                } 
+
+                if (this.xDir == 0) {
+                    this.xDir = this.yDir * changeDir;
+                    this.yDir = 0;
+                } else {
+                    this.yDir = this.xDir * changeDir;
+                    this.xDir = 0;
+                }
+            }
+        }
+
+
+        let targetPos = v2(this.node.position.x + this.x, this.node.position.y + this.y);
+
+        this.player.setPosition(v3(targetPos.x, targetPos.y, 0));
+
+        let results = PhysicsSystem2D.instance.raycast(this.node.position, targetPos, ERaycast2DType.All);
+        if (results) {
+            //console.log(results.length);
             for (let i = 0; i < results.length; i++) {
                 let result = results[i];
                 let collider = result.collider;
-                let distance = result.distance;
-                let hitPoint = result.hitPoint;
-                let hitNormal = result.hitNormal;
+                let point = result.point;
 
-                console.log('ray hit ' + collider + ' from ' + collider.node.name + ' distance ' + distance);
+                console.log('ray hit ' + collider.node.name + ' from ' + collider.node.name + ' at ' + point);
             }
         }
 
